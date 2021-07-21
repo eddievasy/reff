e58242f831febebcb52c75616ae78793bc46bedf
Eduard V <ecv041@student.bham.ac.uk>
2021-07-21 20:46:20 +0100

    Created aliases for URL paths to be used across the Django project so when a path is changed the change won't ripple

--- REFF_Code/entries/urls.py
+++ REFF_Code/entries/urls.py
@@ -5,14 +5,16 @@ from entries.views import entry_list, entry_detail, entry_create, entry_update, 
 app_name = "entries"
 
 urlpatterns = [
-    path('entries', entry_list),
+    # By giving paths names, we can reference the paths across HTML files without worrying
+    # about a ripple effect taking place when changing the path URL below.
+    path('entries', entry_list, name='entry-list'),
     path('entries/', entry_list),
-    path('create/', entry_create),
+    path('create/', entry_create, name='entry-create'),
     # the '<>' syntax is used when generating dynamic URLs;
     # it is also recommended to keep the URL paths
     # containing primary keys (<pk>) at the bottom of the list;
     # or the primary key can be declared as <int: pk>
-    path('entries/<int:pk>/', entry_detail),
-    path('entries/<int:pk>/update/', entry_update),
-    path('entries/<int:pk>/delete/', entry_delete) 
+    path('entries/<int:pk>/', entry_detail, name='entry-detail'),
+    path('entries/<int:pk>/update/', entry_update, name='entry-update'),
+    path('entries/<int:pk>/delete/', entry_delete, name='entry-delete') 
 ]
