2860f7fb4e99791136798a02fa03baab4715a415
Eduard V <ecv041@student.bham.ac.uk>
2021-08-18 20:48:50 +0100

    gave platform more personality whilst maintaining simplicity by changing the fonts as follow: logo -> lobster, titles and body -> epilogue, and for quotes -> jet_brains_mono;

--- REFF_Code/static/jstools/tailwind.config.js
+++ REFF_Code/static/jstools/tailwind.config.js
@@ -11,7 +11,21 @@ module.exports = {
       ]
   },
   theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+            shadows: ["Shadows Into Light"],
+            // To be used for logo
+            cookie: ['Cookie'],
+            // To be used for titles
+            epilogue : ['Epilogue'],
+            // To be used for body
+            source_sans_pro : ['"Source Sans Pro"'],
+            // To be used for quotes
+            jet_brains_mono : ['"JetBrains Mono"'],
+            // To use for logo
+            lobster: ['Lobster'],
+        }
+      },
   },
   variants: {},
   plugins: [],
