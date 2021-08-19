fcb2a8201a08cf20d4e1a9dc519072a23bcf67fc
Eduard V <ecv041@student.bham.ac.uk>
2021-08-19 20:41:40 +0100

    updated short_url generator so it only includes digits and lowercase ascii characters (no capital letters included);

--- REFF_Code/algorithms/random_url_string.py
+++ REFF_Code/algorithms/random_url_string.py
@@ -1,7 +1,7 @@
 import string
 import random
 
-# All characters used: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-# That is a total of 3,521,614,606,208 possible 7-character combinations
-def url_generator(size=7, chars=string.digits + string.ascii_letters):
+# All characters used: 'abcdefghijklmnopqrstuvwxyz0123456789' -> that is a total of 34,359,738,368 permutations (order important: yes, repetition allowed: yes)
+# When using capital letters too we get a total of 3,521,614,606,208 possible permutations (order important: yes, repetition allowed: yes), however we've decided not to use capital letters
+def url_generator(size=7, chars=string.digits + string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
