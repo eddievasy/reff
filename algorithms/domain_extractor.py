7308307168de6f9692d018d86cbf4e4bede16e39
Eduard V <ecv041@student.bham.ac.uk>
2021-08-22 00:26:03 +0100

    ran 'git rm --cached */__pycache__/*' to stop github from constantly telling me the pycache paths are not being tracked;

--- algorithms/domain_extractor.py
+++ algorithms/domain_extractor.py
@@ -7,5 +7,5 @@ def url_parser(long_url):
     return domain
 
 
-print(url_parser("https://www.google.com/search?q=how+amazing+is+life&rlz=1C5CHFA_enGB963GB963&oq=how+amazing+is+life&aqs=chrome..69i57j0i512l9.2338j0j7&sourceid=chrome&ie=UTF-8"))
+# print(url_parser("https://www.google.com/search?q=how+amazing+is+life&rlz=1C5CHFA_enGB963GB963&oq=how+amazing+is+life&aqs=chrome..69i57j0i512l9.2338j0j7&sourceid=chrome&ie=UTF-8"))
 
