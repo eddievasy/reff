5dfe526cfdeab089ed21c80f558cd13f96fac1b9
Eduard V <ecv041@student.bham.ac.uk>
2021-08-24 22:08:35 +0100

    added the 'Like' model to the admin view;

--- entries/admin.py
+++ entries/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Entry, Review
+from .models import User, Entry, Review, Like
 
 admin.site.register(User)
 admin.site.register(Entry)
 admin.site.register(Review)
+admin.site.register(Like)
 
